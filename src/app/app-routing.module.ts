e4c2b1e80da17a461112f7fa7260468fe7a2aee9
Qusz <de.gorchakov@gmail.com>
2023-07-07 11:48:51 +0300

    add routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { MainPageComponent } from './components/pages/main-page/main-page.component';
+import { DownloadPageComponent } from './components/pages/download-page/download-page.component';
+
+const routes: Routes = [
+  { path: '', component: MainPageComponent },
+  { path: 'download', component: undefined, children: [
+      { path: ':id', component: DownloadPageComponent }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
