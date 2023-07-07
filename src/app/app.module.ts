951591bdd19d48c46da5e5274c1638ceba000f4a
Qusz <de.gorchakov@gmail.com>
2023-07-07 11:39:05 +0300

    add footer component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DefaultLayoutComponent } from './components/default-layout/default-layout/default-layout.component';
 import { LogoComponent } from './components/logo/logo.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DefaultLayoutComponent,
-    LogoComponent
+    LogoComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
