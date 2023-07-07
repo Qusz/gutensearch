b193064928ea9ec46b37347134bc408f2a73d737
Qusz <de.gorchakov@gmail.com>
2023-07-07 11:37:27 +0300

    add logo component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DefaultLayoutComponent } from './components/default-layout/default-layout/default-layout.component';
+import { LogoComponent } from './components/logo/logo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DefaultLayoutComponent
+    DefaultLayoutComponent,
+    LogoComponent
   ],
   imports: [
     BrowserModule,
